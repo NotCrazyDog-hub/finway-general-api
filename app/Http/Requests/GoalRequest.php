db5bcd2d153dcd39d1d69641ee22258c76794696
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-02-18 15:10:32 -0300

    :sparkles: feat: adiciona regras nos campos em GoalRequest

--- app/Http/Requests/GoalRequest.php
+++ app/Http/Requests/GoalRequest.php
@@ -11,7 +11,7 @@ class GoalRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class GoalRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|string|max:191',
+            'steps' => 'required|array|min:1',
+            'steps.*' => 'string|min:3|max:191'
         ];
     }
 }
