24ed3ccd64a50071b8b80070f969a5b25aa51f0f
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-02-19 18:50:16 -0300

    :card_file_box: chore: define colunas na tabela cash_boxes

--- database/migrations/2026_02_18_211300_create_cash_boxes_table.php
+++ database/migrations/2026_02_18_211300_create_cash_boxes_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('cash_boxes', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('name');
+            $table->decimal('amount', 10, 2);
             $table->timestamps();
         });
     }
