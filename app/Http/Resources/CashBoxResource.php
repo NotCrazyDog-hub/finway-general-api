176274320c0dad7ab70c4a21d43d76054850e312
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-02-19 19:24:22 -0300

    :sparkles: feat: mapeia campos de retorno no CashBoxResource

--- app/Http/Resources/CashBoxResource.php
+++ app/Http/Resources/CashBoxResource.php
@@ -14,6 +14,11 @@ class CashBoxResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+         return [
+            'cashBoxesId' => $this->id,
+            'name' => $this->name,
+            'amount' => $this->amount,
+            'createdAt' => $this->created_at
+        ];
     }
 }
