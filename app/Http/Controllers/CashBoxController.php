ff1f3acb2c40dec31717bcdb2e0b99d96191296f
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-02-19 18:27:54 -0300

    :recycle: refactor: regera CashBoxController com flag --api

--- app/Http/Controllers/CashBoxController.php
+++ app/Http/Controllers/CashBoxController.php
@@ -2,9 +2,48 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CashBox;
 use Illuminate\Http\Request;
 
 class CashBoxController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CashBox $cashBox)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CashBox $cashBox)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CashBox $cashBox)
+    {
+        //
+    }
 }
