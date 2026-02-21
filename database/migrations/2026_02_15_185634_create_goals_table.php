674d56a96382dc7fdad0d8367342b57ad0660063
NotCrazyDog-hub <pedro.oliveira.ncd@gmail.com>
2026-02-21 15:41:21 -0300

    :card_file_box: chore: ajusta regra onDelete para cascade nas tabelas goals e cash_boxes

--- database/migrations/2026_02_15_185634_create_goals_table.php
+++ database/migrations/2026_02_15_185634_create_goals_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('goals', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->text('steps');
             $table->timestamps();
